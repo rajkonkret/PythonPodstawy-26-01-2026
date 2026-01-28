03e7b046b2479a0b4466dd2518996e08ad3a53ba
radek <rajkonkret660@gmail.com>
2026-01-28 12:07:48 +0100

    [3 9] update petle_zad1.py

--- day3/petle_zad1.py
+++ day3/petle_zad1.py
@@ -29,3 +29,57 @@ for _ in range(6):
 
 print(lista_wynik)
 # [28, 36, 40, 42, 14, 5]
+
+for i in range(10):
+    if i % 2 == 0:  # modulo, reszta z dzielenia
+        print(i, "parzysta")
+
+lista3 = []
+for i in range(10):
+    if i % 2 == 0:
+        lista3.append(i)
+
+print(lista3)  # [0, 2, 4, 6, 8]
+
+# list comprehensions
+lista3 = [j for j in range(10) if j % 2 == 0]
+print(lista3)  # [0, 2, 4, 6, 8]
+
+for c in lista3:  # podstawi kolejne elementy z listy
+    print(c)
+
+lista_nazwy = ["Ala", "Tomek", "Zenek", "Basia"]
+for p in lista_nazwy:
+    print(p)
+# Ala
+# Tomek
+# Zenek
+# Basia
+
+for c in lista3:
+    if c > 4:
+        print(c, "większe do 4")
+    elif c == 4:
+        print(c, "równe 4")
+    else:
+        print(c, "mniejsze od 4")
+
+    print(c)
+print("Po zakońćzeniu pętli")
+# 0 mniejsze od 4
+# 0
+# 2 mniejsze od 4
+# 2
+# 4 równe 4
+# 4
+# 6 większe do 4
+# 6
+# 8 większe do 4
+# 8
+# Po zakońćzeniu pętli
+
+for i in range(10, 0, -2):  # (start, stop, krok) ujemny krok, licznik w dół
+    print(i)
+
+for i in range(-10, 0):
+    print(i)
