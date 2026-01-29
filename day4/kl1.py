434f93cb96875bf210e4a188f61ae1e2f04d8df5
radek <rajkonkret660@gmail.com>
2026-01-29 15:50:03 +0100

    [4 27] update pakiet

--- day4/kl1.py
+++ day4/kl1.py
@@ -11,6 +11,21 @@ class Human:
     """
     Klasa Human opisująca czlowieka w pythonie
     """
+    imie = ""
+    wiek = None
+    plec = "k"
+
+    # self - obiekt
+    def powitanie(self):
+        print(f"Nazywam się: {self.imie}")
+        # print(f"Nazywam się: {cz1.imie}")
+
+    def ruszaj(self):
+
+        if self.plec == "m":
+            print("Ruszył-em w drogę")
+        else:
+            print("Ruszył-am w drogę")
 
 
 print(Human.__doc__)  # dokumentacja
@@ -20,3 +35,40 @@ print(Human.__doc__)  # dokumentacja
 # cd .\day4\ - wejscie do katalogu day4
 # pydoc -b
 # pydoc -w kl1
+
+# tworzenie obiektów
+cz1 = Human()
+print(cz1)  # <__main__.Human object at 0x0000019E188857F0>
+
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+#
+# None
+# k
+
+cz1.imie = "Radek"
+cz1.wiek = 50
+cz1.plec = "m"
+
+print(cz1.imie)  # Radek
+print(cz1.wiek)  # 50
+print(cz1.plec)  # m
+
+cz2 = Human()
+cz2.imie = "Anna"
+cz2.wiek = 45
+
+print(cz2.imie)  # Anna
+print(cz2.wiek)  # 45
+print(cz2.plec)  # k
+
+cz2.powitanie()
+cz1.powitanie()
+# Nazywam się: Anna
+# Nazywam się: Radek
+
+cz1.ruszaj()
+cz2.ruszaj()
+# Ruszył-em w drogę
+# Ruszył-am w drogę
