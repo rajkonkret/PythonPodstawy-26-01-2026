232c12e4e4fa8832db3483ef81d7e0a5ae9eb241
radek <rajkonkret660@gmail.com>
2026-01-29 15:30:58 +0100

    [4 26] update pakiet

---
+++ day4/kl1.py
@@ -0,0 +1,22 @@
+# klasa - przepis, szablon
+# cechy (zmienne)
+# metody - funkcj w klasie
+# obiekt - instancja klasy
+# klasa musi zostac najpierw zadeklarowana
+# tworzenie obiektu uruchamia funkcje inicjalizującą (konsrtruktor) __init__
+# paradygmanty -> hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+# PascalCase, UpperCamelCase
+class Human:
+    """
+    Klasa Human opisująca czlowieka w pythonie
+    """
+
+
+print(Human.__doc__)  # dokumentacja
+# Klasa Human opisująca czlowieka w pythonie
+# print(print.__doc__)
+# pydoc - dokumentacja
+# cd .\day4\ - wejscie do katalogu day4
+# pydoc -b
+# pydoc -w kl1
