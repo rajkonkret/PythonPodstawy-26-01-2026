9515c7197641be6dcc8c945aba085af8182422df
radek <rajkonkret660@gmail.com>
2026-01-30 12:32:49 +0100

    [5 16] update baza_danych

---
+++ day5/baza1.py
@@ -0,0 +1,20 @@
+# baza danych - system przechowywania danych
+# silnik - mechanizm zarządzania danymi w bazie
+# bazy relacyjne, nierelacyjne
+# sql, nosql
+# mysql, postgress, mssql, oracle, sqlite
+
+import sqlite3
+
+try:
+    conn = sqlite3.connect("baza_danych.db")
+    c = conn.cursor()
+    print("Baza danych została podłączona")
+except sqlite3.Error as e:
+    print("Bład podłączania z bazą:", e)
+finally:
+    if conn:
+        conn.close()
+        print("Połączenie zostało zamknięte")
+# Baza danych została podłączona
+# Połączenie zostało zamknięte
